8319ef9a1e30876b02f4ec7b440bda843d41b5b5
Vishal74674 <vkvishal.5mk@gmail.com>
2023-03-13 12:48:04 +0530

    url added

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ import Config from "config";
 import Routes from "./routes";
 import Server from "./common/server";
 
-const dbUrl = "mongodb+srv://booking:booking123@cluster0.4olhgpv.mongodb.net/TESTING_MODE";
+const dbUrl = `mongodb://${Config.get("databaseHost")}:${Config.get(
+  "databasePort"
+)}/${Config.get("databaseName")}`;
 const server = new Server()
   .router(Routes)
   .configureSwagger(Config.get("swaggerDefinition"))
